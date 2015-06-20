8a964d0a47d3d5e02766a5f1baac82bf60502194
U.SARVANI <U.SARVANI@SARVANI>
2015-06-20 12:38:10 +0700

    commit changes

--- src/JSIntro.js
+++ src/JSIntro.js
@@ -1,23 +1,40 @@
 
 exports.Sum = function(num1, num2){
+
+
+    return num1 + num2;
 }
 
 exports.SumOfArray = function(arrayOfNums){
-
+    var sum = 0;
+    for (i = 0; i < arrayOfNums.length; i++)
+        sum = sum + arrayOfNums[i];
+    return sum;
 }
 
 // Sum only the unique numbers in the array.
 // Ex: If array is [2,3,3,2], the sum is 2+3=5
 
-exports.SumOfUniqueNumbers = function(arrayOfNums){
+exports.SumOfUniqueNumbers = function (arrayOfNums) {
+    var sum = 0;
+    for (i = 0; i < arrayOfNums.length; i++) {
+        for (j = i + 1; j < arrayOfNums.length; j++) {
+            if (arrayOfNums[i] == arrayOfNums[j]) {
+                arrayOfNums[j] = 0;
+            }
+        }
+    }
+    for (i = 0; i < arrayOfNums.length; i++)
+        sum = sum + arrayOfNums[i];
+    return sum;
+
 }
 
 exports.ReverseString = function(str){
-
-}
-
-
-exports.ReverseArrayOfStrings = function(arrayOfStrings){
-
-
+    var temp = "";
+    for(i=str.length-1;i>=0;i--)
+        {
+        temp = temp + str[i];
+    }
+    return temp;
 }
--- src/contacts/routes/contacts.js
+++ src/contacts/routes/contacts.js
@@ -2,16 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET contacts */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
+    res.json(array[req.params.id]);
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
+    count++;
+    array.push(req.body);
+    res.json(count - 1);
   console.log(req.body);
 });
 
-router.put('/:id', function(req, res, next) {
+router.put('/:id', function (req, res, next) {
+    array[req.params.id].firstName = req.body.firstName;
+    return res.json(array[req.params.id]);
   console.log(req.body);
 
 });
+
+
+});
+
 
 module.exports = router;
